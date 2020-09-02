f662ee6c487c2ba359e4eac39f88d1b680611b78
Shivay10th <62726735+Shivay10th@users.noreply.github.com>
2020-09-02 16:26:13 +0530

    Updates

---
+++ precache-manifest.4828cc8f7260a79eb35bc3a3f8b51d2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2ccb2ce515b31ac95172a2536ed67b",
+    "url": "/Monster-Robolex/index.html"
+  },
+  {
+    "revision": "da11118152e7d36677ac",
+    "url": "/Monster-Robolex/static/css/main.a6c07fb6.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/Monster-Robolex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Robolex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da11118152e7d36677ac",
+    "url": "/Monster-Robolex/static/js/main.79b5b480.chunk.js"
+  },
+  {
+    "revision": "60a09e9d6f66965a08bc",
+    "url": "/Monster-Robolex/static/js/runtime-main.88214793.js"
+  }
+]);
